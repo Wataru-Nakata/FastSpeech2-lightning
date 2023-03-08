96f1363b5578a10430e3f6c659230763df062a52
Wataru Nakata <wataru9871@gmail.com>
2023-03-08 20:35:44 +0900

    added prprocessing code

--- src/dataset/jsut.py
+++ src/dataset/jsut.py
@@ -2,7 +2,7 @@ import torch
 import torchaudio
 from pathlib import Path
 from .preprocessDataset import PreprocessDataset
-class JSUTDataset(PreprocessDataset):
+class JSUTDataset(torch.utils.data.Dataset):
     '''
     JSUT dataset class for TTS
     JSUT can be download from https://sites.google.com/site/shinnosuketakamichi/publication/jsut?authuser=0
@@ -12,12 +12,14 @@ class JSUTDataset(PreprocessDataset):
             self,
             root: Path,
             alignmnet_root: Path,
+            time_scale_factor:float = 100e-9
         ) -> None:
         self.root = root
         self.wav_files = list(self.root.glob('**/*.wav'))
         self.transcript_files = list(self.root.glob('**/transcript_utf8.txt'))
-        self.alignment_files = list(self.root.glob('**/*.wav'))
+        alignment_files = list(self.root.glob('**/*.wav'))
         self.current_wav_index = 0
+        self.time_scale_factor = time_scale_factor
 
         self.transcript = dict()
         for transcript_file in self.transcript_files:
@@ -26,17 +28,33 @@ class JSUTDataset(PreprocessDataset):
                 for line in lines:
                     k,v = line.strip().split(':')
                     self.transcript[k] = v
-    def parse_alignment_file(self):
-        pass
 
-    def __next__(self):
+        self.alignment_file_dict= dict()                    
+        for alignment_file in alignment_files:
+            self.algnment_file_dict[alignment_file.stem] = alignment_file
+
+    def parse_alignment_file(self,file_id:str):
+        alignment_file_path:Path = self.alignment_file_dict[file_id]
+        phones = []
+        starts = []
+        ends = []
+        with alignment_file_path.open() as f:
+            lines = f.readlines()
+            for line in lines:
+                start, end, full_cntext = line.strip().split(' ')
+                starts.append(float(start)*self.time_sclae_factor)
+                end.append(float(start)*self.time_scale_factor)
+                phones.append(phone)
+        return starts, ends, phones
+                
+    def pp_symbols(self):
+        raise NotImplementedError
+    def get_item(self):
         wav_file_path:Path = self.wav_files[self.current_wav_index]
         transcript:str = self.transcript[wav_file_path.stem]
         alignment = self.pares_alignment_file(wav_file_path.stem)
+        audio = torchaudio.load(wav_file_path)
         return wav_file_path,transcript,alignment
-
 
     def __len__(self):
         return len(self.wav_files)
-
-
--- src/dataset/preprocessDataset.py
+++
@@ -1,9 +0,0 @@
-class PreprocessDataset():
-    def __init__(self) -> None:
-        pass
-    def __iter__(self):
-        return self
-    def __len__(self):
-        raise NotImplementedError
-    def __next__(self):
-        raise NotImplementedError
